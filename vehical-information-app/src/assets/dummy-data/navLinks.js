da4ec5e4909d39dc1c14e796290014d570e65879
ramakantaswale001 <ramakantaswale001@gmail.com>
2023-06-08 19:42:31 +0530

    first commit by ramakant

---
+++ vehical-information-app/src/assets/dummy-data/navLinks.js
@@ -0,0 +1,44 @@
+const navLinks = [
+  {
+    path: "/dashboard",
+    icon: "ri-apps-2-line",
+    display: "Dashboard",
+  },
+  {
+    path: "/bookings",
+    icon: "ri-taxi-line",
+    display: "Booking",
+  },
+  {
+    path: "/sell-car",
+    icon: "ri-shopping-bag-line",
+    display: "Sell Cars",
+  },
+  {
+    path: "/settings",
+    icon: "ri-settings-2-line",
+    display: "Registration",
+  },
+  // {
+  //   path: "/login",
+  //   icon: "ri-settings-2-line",
+  //   display: "Login",
+  // },
+  // {
+  //   path: "/loginandregister",
+  //   icon: "ri-settings-2-line",
+  //   display: "Login/Register",
+  // },
+  // {
+  //   path: "/userregistration",
+  //   icon: "ri-settings-2-line",
+  //   display: "User Registration",
+  // },
+  // {
+  //   path: "/ownerregistration",
+  //   icon: "ri-settings-2-line",
+  //   display: "Owner Registration",
+  // },
+];
+
+export default navLinks;
